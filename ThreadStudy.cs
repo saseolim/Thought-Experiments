c01379b5356491d8ca4fd9f3af992e8ab963aa07
사서림 <saseolimit27@gmail.com>
2026-01-24 00:33:41 +0900

    Thread 기초 연습

---
+++ ThreadStudy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreadStudy
+{
+    public struct ThreadHeader
+    {
+        public int mode { get; private set; }
+        public int code { get; private set; }
+
+        public ThreadHeader(int mode, int code)
+        {
+            this.mode = mode;
+            this.code = code;
+        }
+    }
+    public partial class ThreadStudy
+    {
+        public ThreadHeader header;
+        public volatile bool IsRun = false;
+        public volatile bool isEnd = false;
+        public bool IsEnd { get => isEnd; private set => isEnd = value; }
+
+        public ThreadStudy(ThreadHeader header)
+        {
+            this.header = header;
+        }
+        public void Run()
+        {
+            while (!IsRun)
+            {
+                Thread.Sleep(new Random().Next(1, 100));
+            }
+
+            int mode = header.mode;
+            int code = header.code;
+            Console.WriteLine("ThreadHeader Run Method : " + code);
+            switch (mode)
+            {
+                case 0:
+                    Console.WriteLine("Mode 0 selected : " + code);
+                    break;
+                case 1:
+                    Console.WriteLine("Mode 1 selected : " + code);
+                    break;
+                case 2:
+                    Console.WriteLine("Mode 2 selected : " + code);
+                    break;
+                default:
+                    Console.WriteLine("Unknown mode : " + code);
+                    break;
+            }
+            Console.WriteLine();
+            while (!IsRun)
+            {
+                Thread.Sleep(new Random().Next(10, 100));
+            }
+            IsEnd = true;
+        }
+    }
+}
---
+++ ThreadStudyProgram.cs
@@ -0,0 +1,77 @@
+﻿namespace ThreadStudy
+{
+    internal class ThreadStudyProgram
+    {
+        static void Main(string[] args)
+        {
+            Random mode = new Random();
+            List<Thread> threads = new List<Thread>();
+
+            ThreadStudy[] TF = new ThreadStudy[100];
+            for (int i = 0; i < 100; i++)
+            {
+                int this_mode = mode.Next(-1, 3);
+                TF[i] = new ThreadStudy(new ThreadHeader(this_mode, i));
+                threads.Add(new Thread(new ThreadStart(TF[i].Run)));
+                threads[threads.Count - 1].Start();
+            }
+
+            List<int> zero = new List<int>();
+            List<int> one = new List<int>();
+            List<int> two = new List<int>();
+            List<int> def = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                switch (TF[i].header.mode)
+                {
+                    case 0:
+                        zero.Add(i);
+                        break;
+                    case 1:
+                        one.Add(i);
+                        break;
+                    case 2:
+                        two.Add(i);
+                        break;
+                    default:
+                        def.Add(i);
+                        break;
+                }
+            }
+            int count = 0;
+            Console.WriteLine(zero.Count + one.Count + two.Count + def.Count);
+
+            for (int i = 0; i < zero.Count; i++)
+            {
+                TF[zero[i]].IsRun = true;
+                threads[zero[i]].Join();
+                count++;
+            }
+
+            for (int i = 0; i < one.Count; i++)
+            {
+                TF[one[i]].IsRun = true;
+                threads[one[i]].Join();
+                count++;
+            }
+
+            for (int i = 0; i < two.Count; i++)
+            {
+                TF[two[i]].IsRun = true;
+                threads[two[i]].Join();
+                count++;
+            }
+
+            for (int i = 0; i < def.Count; i++)
+            {
+                TF[def[i]].IsRun = true;
+                threads[def[i]].Join();
+                count++;
+            }
+
+            Console.WriteLine("Program End : " + count);
+
+        }
+    }
+}
